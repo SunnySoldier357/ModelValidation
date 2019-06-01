94bf7b6fecc226162fe0742a261fe3d1d3f5783d
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-06-01 08:15:55 -0700

    [ModelValidation] Add MaximumLength & MinimumLength Attributes

---
+++ ModelValidation/MaximumLengthAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace ModelValidation
+{
+    /// <summary>
+    /// This attribute requires that the property has a length less
+    /// than the value specified. The property has to be a string,
+    /// otherwise the property's ToString() method will be used as
+    /// a comparison.
+    /// </summary>
+    public class MaximumLengthAttribute : ValidatablePropertyAttribute
+    {
+        //* Public Properties
+        public int MaximumLength { get; set; }
+
+        //* Constructors
+        public MaximumLengthAttribute(int maximumLength) =>
+            MaximumLength = maximumLength;
+
+        //* Overriden Methods
+        public override bool Validate<T>(T value, string nameofProperty,
+            out string errorMessage)
+        {
+            string strValue = value.ToString();
+
+            bool valid = strValue.Length <= MaximumLength;
+
+            errorMessage = valid ? "" : $"{nameofProperty} too long";
+
+            return valid;
+        }
+    }
+}
---
+++ ModelValidation/MinimumLengthAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace ModelValidation
+{
+    /// <summary>
+    /// This attribute requires that the property has a length more
+    /// than the value specified. The property has to be a string,
+    /// otherwise the property's ToString() method will be used as
+    /// a comparison.
+    /// </summary>
+    public class MinimumLengthAttribute : ValidatablePropertyAttribute
+    {
+        //* Public Properties
+        public int MinimumLength { get; set; }
+
+        //* Constructors
+        public MinimumLengthAttribute(int minimumLength) =>
+            MinimumLength = minimumLength;
+
+        //* Overridden Methods
+        public override bool Validate<T>(T value, string nameofProperty,
+            out string errorMessage)
+        {
+            string strValue = value.ToString();
+
+            bool valid = strValue.Length >= MinimumLength;
+
+            errorMessage = valid ? "" : $"{nameofProperty} too short";
+
+            return valid;
+        }
+    }
+}
