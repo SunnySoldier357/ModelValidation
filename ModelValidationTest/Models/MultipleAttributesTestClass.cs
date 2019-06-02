fc2a06e8495cf53de1e59d33568f92a45208fc05
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-06-01 17:53:33 -0700

    [ModelValidationTest] Add test for Multiple Attributes

---
+++ ModelValidationTest/Models/MultipleAttributesTestClass.cs
@@ -0,0 +1,16 @@
+﻿using ModelValidation;
+
+namespace ModelValidationTest.Models
+{
+    public class MultipleAttributesTestClass : ValidatableObject
+    {
+        //* Public Properties
+        [ContainsCharacter('@')]
+        [MinimumLength(2)]
+        public string Value { get; set; }
+
+        //* Constructors
+        public MultipleAttributesTestClass(string value) => 
+            Value = value;
+    }
+}
---
+++ ModelValidationTest/MultipleAttributesTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ModelValidationTest.Models;
+
+namespace ModelValidationTest
+{
+    [TestClass]
+    public class MultipleAttributesTest
+    {
+        //* Test Methods
+
+        [TestMethod]
+        public void NullValueTest() => testValidValidation(null);
+
+        [TestMethod]
+        public void BothInvalidTest()
+        {
+            var test = new MultipleAttributesTestClass("");
+
+            Assert.IsFalse(test.Validate());
+            Assert.IsTrue(test.Errors.Count == 2);
+            Assert.IsTrue(test.Errors.Contains($"{nameof(test.Value)} is invalid as it" +
+                " does not contain a '@'"));
+            Assert.IsTrue(test.Errors.Contains($"{nameof(test.Value)} too short"));
+        }
+
+        [TestMethod]
+        public void SingleValidTest()
+        {
+            var test = new MultipleAttributesTestClass("@");
+
+            Assert.IsFalse(test.Validate());
+            Assert.IsTrue(test.Errors.Count == 1);
+            Assert.IsTrue(test.Errors.Contains($"{nameof(test.Value)} too short"));
+        }
+
+        [TestMethod]
+        public void BothValidTest() => testValidValidation("sss@sss");
+
+        //* Private Properties
+        private void testValidValidation(string value)
+        {
+            var test = new MultipleAttributesTestClass(value);
+
+            Assert.IsTrue(test.Validate());
+            Assert.IsTrue(test.Errors.Count == 0);
+        }
+    }
+}
