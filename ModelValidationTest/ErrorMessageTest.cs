219729f190e3b725db0450f77be80e29183eb789
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-06-01 18:15:20 -0700

    [ModelValidationTest] Add tests for ErrorMessage

---
+++ ModelValidationTest/ErrorMessageTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ModelValidationTest.Models;
+
+namespace ModelValidationTest
+{
+    [TestClass]
+    public class ErrorMessageTest
+    {
+        [TestMethod]
+        public void NullTest()
+        {
+            var test = new ErrorMessageTestClass(null);
+
+            Assert.IsTrue(test.Validate());
+            Assert.IsTrue(test.Errors.Count == 0);
+        }
+
+        [TestMethod]
+        public void InvalidTest()
+        {
+            var test = new ErrorMessageTestClass("123456789");
+
+            Assert.IsFalse(test.Validate());
+            Assert.IsTrue(test.Errors.Count == 1);
+            Assert.IsTrue(test.Errors[0] == ErrorMessageTestClass.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ ModelValidationTest/Models/ErrorMessageTestClass.cs
@@ -0,0 +1,18 @@
+﻿using ModelValidation;
+
+namespace ModelValidationTest.Models
+{
+    public class ErrorMessageTestClass : ValidatableObject
+    {
+        //* Constants
+        public const string ERROR_MESSAGE = "This is a test message";
+
+        //* Public Properties
+        [MaximumLength(2, ErrorMessage = ERROR_MESSAGE)]
+        public string Value { get; set; }
+
+        //* Constructors
+        public ErrorMessageTestClass(string value) =>
+            Value = value;
+    }
+}
